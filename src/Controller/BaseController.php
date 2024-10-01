fb11a06a72c4be8554a119e9d46f598e35cf4884
Elise <elise.bonnet1@ecoles-epsi.net>
2024-10-01 11:45:24 +0200

    Questions 4 et 5

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AjoutBonbonType;
+use Symfony\Component\HttpFoundation\Request;
 
 class BaseController extends AbstractController
 {
@@ -12,6 +14,24 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/ajoutBonbon', name: 'app_ajoutBonbon')]
+    public function ajoutBonbon(Request $request): Response
+    {
+        $form = $this->CreateForm(AjoutBonbonType::class);
+        
+        if ($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()) {
+                $this->addFlash('notice', 'Message envoyé');
+                return $this->redirectToRoute('app_ajoutBonbon');
+            }
+        }
+
+        return $this->render('base/ajoutBonbon.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
